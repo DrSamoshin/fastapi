d7e4fc96ec37c2700f75e6c2e707c87a76781519
Siarhei Samoshyn <gds.grey@gmail.com>
2024-08-14 23:30:28 +0300

    add DB

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
-from fastapi import FastAPI
+from typing import Annotated
 
-app = FastAPI()
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+from contextlib import asynccontextmanager
+
+from database import create_tables, delete_tables
 
 
-@app.get("/home")
-def get_home():
-    return {"data": "Hello"}
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print('DB is deleted')
+    await create_tables()
+    print('DB is created')
+    yield
+    print('app is off')
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+class STaskAdd(BaseModel):
+    name: str
+    description: str | None = None
+
+
+class STask(STaskAdd):
+    id: int
+
+
+tasks = []
+
+
+@app.post("/tasks")
+async def add_tasks(task: Annotated[STaskAdd, Depends()]):
+    tasks.append(task)
+    return {"ok": True}
+
+
+@app.get("/tasks")
+def get_tasks():
+    task = STask(name="First")
+    return {"data": task}
